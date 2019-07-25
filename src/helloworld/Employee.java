b7e3657f92631f02cfcd08bd9ecf14bb754eef81
sudarshan agolla <sudarshan agolla@DESKTOP-4UR5ELF>
2019-07-25 22:30:50 +0530

    add employee into helloworld

---
+++ src/helloworld/Employee.java
@@ -0,0 +1,22 @@
+package helloworld;
+
+public class Employee {
+	
+	private Integer eno;
+	private String ename;
+	public Integer getEno() {
+		return eno;
+	}
+	public void setEno(Integer eno) {
+		this.eno = eno;
+	}
+	public String getEname() {
+		return ename;
+	}
+	public void setEname(String ename) {
+		this.ename = ename;
+	}
+	
+	
+
+}
